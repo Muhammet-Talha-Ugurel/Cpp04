7c4939a1e1c8b94d5dcfa56a56940c3049025c56
Muhammed Talha Ugurel <muhammedtalhaugurel@gmail.com>
2024-03-25 05:39:00 +0300

    bitti ins

---
+++ ex00/Cat.cpp
@@ -0,0 +1,30 @@
+#include "Cat.hpp"
+
+Cat::Cat(void)
+{
+	std::cout << "Default Cat constructor called" << std::endl;
+	this->type = "Cat";
+}
+
+Cat::Cat(const Cat &src)
+{
+	std::cout << "Cat copy constructor called" << std::endl;
+	this->type = src.getType();
+}
+
+Cat &Cat::operator=(const Cat &src)
+{
+	// std::cout << "Cat assignment operator called" << std::endl;
+	this->type = src.getType();
+	return (*this);
+}
+
+void Cat::makeSound(void) const
+{
+	std::cout << "Meow" << std::endl;
+}
+
+Cat::~Cat(void)
+{
+	std::cout << "Default Cat destructor called" << std::endl;
+}
---
+++ ex02/Dog.cpp
@@ -0,0 +1,35 @@
+#include "Dog.hpp"
+
+Dog::Dog(void)
+{
+	std::cout << "Default Dog constructor called" << std::endl;
+	this->type = "Dog";
+	this->DogBrain = new Brain();
+}
+
+Dog::Dog(const Dog &src)
+{
+	std::cout << "Dog copy constructor called" << std::endl;
+	this->DogBrain = new Brain();
+	*this = src;
+}
+
+Dog &Dog::operator=(const Dog &src)
+{
+	// std::cout << "Dog assignment operator called" << std::endl;
+	this->type = src.getType();
+	delete this->DogBrain;
+	this->DogBrain = new Brain(*src.DogBrain);
+	return (*this);
+}
+
+void Dog::makeSound(void) const
+{
+	std::cout << "Bark!" << std::endl;
+}
+
+Dog::~Dog(void)
+{
+	std::cout << "Default Dog destructor called" << std::endl;
+	delete this->DogBrain;
+}
